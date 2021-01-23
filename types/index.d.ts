48acbf43bd151e05b2021efc371097a784077594
arvinxx <arvinx@foxmail.com>
2021-01-24 07:30:44 +0800

    :label: refactor(types): 优化类型定义文件

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,9 @@
 declare module '*.less';
-declare module '*.ico';
 declare module '*.png';
-declare module '*.jpg';
+declare module '*.svg' {
+  export function ReactComponent(
+    props: React.SVGProps<SVGSVGElement>,
+  ): React.ReactElement;
+  const url: string;
+  export default url;
+}
