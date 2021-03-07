f6bd7fc15c49ef218c5956c31c4b1d048906c3b9
arvinxx <arvinx@foxmail.com>
2021-03-07 11:15:16 +0800

    :bulb: docs: 补充说明

--- config/config.ts
+++ config/config.ts
@@ -18,13 +18,14 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-  // React Dev tools 暂时没法修复 (Electron 版本 >= 9.0)
-  // 所以使用 react-devtools 独立版本
-  // 需要添加 <script src="http://localhost:8097"></script>
-  // Ref: https://github.com/electron/electron/issues/23662
+  /**
+   * React Dev tools 暂时没法修复 (Electron 版本 >= 9.0)
+   * 所以使用 react-devtools 独立版本
+   * 需要添加 <script src="http://localhost:8097"></script>
+   * @see https://github.com/electron/electron/issues/23662
+   */
   scripts: isDev ? ['http://localhost:8097'] : undefined,
   routes,
-  outputPath: '../../release',
   /**
    * electron 默认情况下应该其实就是 mpa 环境
    * 但是直接使用 umi 的 mpa 方法会丢失全局 Layout
@@ -36,6 +37,9 @@ export default defineConfig({
         htmlSuffix: true,
         dynamicRoot: true,
       },
+
+  // renderer 部分相对根目录的输出路径为 ../..
+  outputPath: '../../release',
   electronBuilder: {
     routerMode: isDev ? 'hash' : 'browser',
     outputDir: 'release',
