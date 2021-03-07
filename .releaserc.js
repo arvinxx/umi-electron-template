0772d6416bff977936da760eb871663f1977e388
arvinxx <arvinx@foxmail.com>
2021-03-07 11:48:59 +0800

    :wrench: chore: 调整 release 配置

--- .releaserc.js
+++ .releaserc.js
@@ -1,43 +1,11 @@
+const {
+  createConfig,
+} = require('semantic-release-config-gitmoji/lib/createConfig');
+const config = createConfig({
+  changelogTitle: '# Umi Electron Template 更新日志',
+  githubAssets: ['release'],
+});
+
 module.exports = {
-  plugins: [
-    //负责解析 commit
-    [
-      '@semantic-release/commit-analyzer',
-      {
-        // 自定义配置，如果不填则是默认的 conventional-changelog-angular
-        config: 'conventional-changelog-gitmoji-config',
-      },
-    ],
-    [
-      '@semantic-release/release-notes-generator', //此处生成 github-release 的日志
-      {
-        //指定配置，这里才是负责生成日志的，也就是说，如果自定义了writerOpts，只有在这里写才会生效
-        config: 'conventional-changelog-gitmoji-config',
-      },
-    ],
-    [
-      '@semantic-release/changelog', //此处会调用上一个插件生成的新增日志，然后合并到原有日志中
-      {
-        changelogFile: 'CHANGELOG.md',
-        changelogTitle: '# Umi Electron Template 更新日志',
-      },
-    ],
-    '@semantic-release/npm', // 自动更新版本号,如果是 npm 模块则会触发发布流程
-    // 在 github 发布 Release 版本
-    [
-      '@semantic-release/github',
-      {
-        assets: ['release'],
-      },
-    ],
-    // 推送代码回 git
-    [
-      '@semantic-release/git',
-      {
-        assets: ['CHANGELOG.md', 'package.json'],
-        message:
-          ':bookmark: chore(release): ${nextRelease.gitTag} [skip ci] \n\n${nextRelease.notes}',
-      },
-    ],
-  ],
+  extends: [config],
 };
