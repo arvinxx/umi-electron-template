8b95be0d6183d660b48b59ac4eb2a951eb192216
arvinxx <arvinx@foxmail.com>
2021-03-07 11:58:21 +0800

    :wrench: chore: 调整 release 配置

--- .releaserc.js
+++ .releaserc.js
@@ -1,6 +1,5 @@
-const {
-  createConfig,
-} = require('semantic-release-config-gitmoji/lib/createConfig');
+const createConfig = require('semantic-release-config-gitmoji/lib/createConfig');
+
 const config = createConfig({
   changelogTitle: '# Umi Electron Template 更新日志',
   githubAssets: ['release'],
