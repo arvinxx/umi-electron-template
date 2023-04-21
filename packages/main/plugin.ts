6bae9f1457c2b3e802ccba4629439fa2888cdc0c
arvinxx <arvinx@foxmail.com>
2023-04-22 00:27:17 +0800

    :fire: refactor: 清理无用代码

--- packages/main/plugin.ts
+++ packages/main/plugin.ts
@@ -1,5 +1,5 @@
 import { IApi } from '@umijs/max';
 
 export default (api: IApi) => {
-  api.logger.info('Electron 端');
+  api.logger.info('启动 Electron 端,上述ip地址可忽略...');
 };
--- packages/main/src/services/UserService.ts
+++ packages/main/src/services/UserService.ts
@@ -1,27 +1,3 @@
-import { User } from '../models';
+import { ServiceModule } from './index';
 
-import { event, ServiceModule } from './index';
-
-export default class UserService extends ServiceModule {
-  get model() {
-    return this.app.database.getRepository(User)!;
-  }
-
-  /**
-   * 创建对象
-   * @param name
-   * @param surname
-   */
-  @event('/user/add')
-  async insert(name: string, surname: string): Promise<User> {
-    return this.model.save({ name, surname });
-  }
-
-  /**
-   * 查找所有
-   */
-  @event('/user/find-all')
-  async finAll(): Promise<User[]> {
-    return this.model.find();
-  }
-}
+export default class UserService extends ServiceModule {}
